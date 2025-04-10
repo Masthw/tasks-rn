bd3d5a76502ea0f32669f3f993b1bb1e12921ade
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-04-10 15:13:20 -0300

    :art: style: add more colors to commmonStyles

--- src/commonStyles.ts
+++ src/commonStyles.ts
@@ -2,6 +2,9 @@ const commonStyles = {
   fontFamily: 'Lato-Bold',
   colors: {
     today: '#B13b44',
+    tomorrow: '#C9742E',
+    week: '#15721E',
+    month: '#1631BE',
     secondary: '#FFF',
     mainText: '#222',
     subText: '#555',
